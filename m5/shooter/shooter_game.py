4fd8c320cc2288c9c7aaafd6f3dc21a4cb1c9038
Markiian Yurga <markianurga5@gmail.com>
2024-02-24 09:41:51 +0200

    початок роботи на шутером

---
+++ m5/shooter/shooter_game.py
@@ -0,0 +1,8 @@
+#Створи власний Шутер!
+from pygame import *
+from pygame.sprite import Sprite
+from pygame.transform import scale, flip
+from pygame.image import load
+from random import randint
+
+
