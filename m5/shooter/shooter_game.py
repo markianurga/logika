cd41f70bc3dffc638fe417e3ed5f337639242ab3
Markiian Yurga <markianurga5@gmail.com>
2024-03-02 10:32:49 +0200

    додали прибульців

--- m5/shooter/shooter_game.py
+++ m5/shooter/shooter_game.py
@@ -10,6 +10,12 @@ mixer.music.load("m5/shooter/space.ogg")
 mixer.music.set_volume(0)
 mixer.music.play(-1)
 
+font.init()
+f1 = font.Font(None, 36) 
+
+ckore = 0
+lose = 0
+
 height = 500
 width = 700
 
@@ -41,12 +47,42 @@ class Player(GameSprite):
                    
         if pressed[K_RIGHT]:
             self.rect.x += self.speed       
+            
+    def faer(self):
+        pass     
+
+class Enemy(GameSprite):
+    def update(self):
+        self.rect.y += self.speed
+        if self.rect.y > 500:
+            self.rect.y = 0
+            self.rect.x = randint(0, 620)
+        
+            
+rocet = Player("m5/shooter/rocket.png", 300, 400, 80, 100, 12 )
+
+enemy0 = Enemy("m5/shooter/ufo.png", randint(0, 620), 0, 80, 50, randint(1, 5) )
+enemy1 = Enemy("m5/shooter/ufo.png", randint(0, 620), 0, 80, 50, randint(1, 5) )
+enemy2 = Enemy("m5/shooter/ufo.png", randint(0, 620), 0, 80, 50, randint(1, 5) )
+enemy3 = Enemy("m5/shooter/ufo.png", randint(0, 620), 0, 80, 50, randint(1, 5) )
+enemy4 = Enemy("m5/shooter/ufo.png", randint(0, 620), 0, 80, 50, randint(1, 5) )
+enemy5 = Enemy("m5/shooter/ufo.png", randint(0, 620), 0, 80, 50, randint(1, 5) )
+
+enemys = sprite.Group(enemy0, enemy1, enemy2, enemy3)
 
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
             
-    window.blit(background, [0, 0])
+    if not finish:   
+        window.blit(background, [0, 0])
+        txt_lose = f1.render("пропушкно " +str(lose), 1, [255,255,255])
+        window.blit(txt_lose, [0, 16])
+        
+        rocet.reset()
+        rocet.update()
+        enemys.draw(window)
+        enemys.update()
     display.update()
     clock.tick(fps)
